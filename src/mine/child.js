899e673f64683af90cc16b52d7de8959b176be87
Muhammad Umair <umair.bscs3643@iiu.edu.pk>
2021-06-28 13:34:10 +0500

    Update child.js

--- src/mine/child.js
+++ src/mine/child.js
@@ -1,67 +1,82 @@
-import React,{useContext,useState} from "react";
-import {TransactionContext} from "./transContext";
+import React, { useState, useContext } from 'react';
+import { TransactionContext } from './context';
 
 function Child() {
 
-   
-    let {transactions,addTransaction}=useContext(TransactionContext);
-    let [newDesc,setDesc]=useState("");
-    let [newAmount,setAmount]=useState(0);
+    const { transactions, addTransaction } = useContext(TransactionContext);
 
+    // let [transactions, setTransaction] = useState(transactions);
+    let [newDesc, setNewDesc] = useState('');
+    let [newAmount, setNewAmount] = useState(0);
 
-    const handleAdditoin=(event)=>{
+    const handleSubmit = (event) => {
         event.preventDefault();
-        addTransaction({
-            amount:newAmount,
-            desc:newDesc
-        })
+        addTransaction({ amount: Number(newAmount), desc: newDesc })
+    }
 
+    const getIncome = () => {
+        let income = 0;
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount > 0)
+                income += transactions[i].amount
+        }
+        return income;
+    }
+
+    const getExpense = () => {
+        let expense = 0;
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount < 0)
+                expense += transactions[i].amount
+        }
+        return expense;
     }
 
     return (
         <div className="container">
-
             <h1 className="text-center">Expense Tracker</h1>
-            <h3>Your Balance <br /> $700</h3>
+            <h3>Your balance <br /> {getIncome() + getExpense()} </h3>
 
-            <div className="expense-container">
-                <h3>INCOME <br /> $700</h3>
-                <h3>EXPENSE <br /> $7</h3>
+            <div className="total-expense-container">
+                <h3>Income <br /> {getIncome()} </h3>
+                <h3>Expense <br /> {getExpense()}</h3>
             </div>
-            <h3>History</h3>
+
+            <h4>History</h4>
             <hr />
 
-
-
-
-            <ul className="transaction-list">
-                {transactions.map((transObj, ind) => {
-                    return (<li key={ind}>
-                        <span>{transObj.desc}</span>
-                        <span>{transObj.amount}</span>
-                    </li>
-
+            <ul className="expense-list">
+                {transactions.map((transaction, ind) => {
+                    return (
+                        <li key={ind}>
+                            <span> {transaction.desc} </span>
+                            <span> {transaction.amount} </span>
+                        </li>
                     )
-
                 })}
             </ul>
 
-            <h3>Add new Transaction</h3>
+            <h4>Add New Transaction</h4>
             <hr />
 
-            <form className="transaction-form" onSubmit={handleAdditoin}>
+            <form className="transaction-form" onSubmit={handleSubmit}>
+                <label>
+                    Enter Description: <br />
+                    <input type="text" required onChange={(e) => setNewDesc(e.target.value)} />
+                </label>
 
-                <label>Text</label><br />
-                <input type="text" placeholder="Enter text..." onChange={(ev)=>setDesc(ev.target.value)} required />
                 <br />
-                <br />
-                <label>Amount<br />(negative-expense, positive-income)</label><br />
-                <input type="text" placeholder="Enter Amount..." onChange={(ev)=>setAmount(ev.target.value)} required />
-                <br /><br />
+
+                <label>
+                    Enter Amount: <br />
+                    <input type="number" required onChange={(e) => setNewAmount(e.target.value)} />
+                </label>
+
+                <br /> <br />
+
                 <input type="submit" value="Add Transaction" />
 
             </form>
-
         </div>
     );
 }
